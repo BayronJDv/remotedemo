35e501c8de8e6aa540e74802aba3092163f01e65
Bayron Jojoa <bayron.jojoa.dv@gmail.com>
2025-11-23 23:12:03 -0500

    si

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -4,8 +4,19 @@ import { pluginModuleFederation } from '@module-federation/rsbuild-plugin';
 import moduleFederationConfig from './module-federation.config';
 
 export default defineConfig({
-  plugins: [pluginReact(), pluginModuleFederation(moduleFederationConfig)],
+  plugins: [
+    pluginReact(), 
+    pluginModuleFederation(moduleFederationConfig)
+  ],
   server: {
     port: 3001,
+  },
+  // ESTA ES LA PARTE NUEVA QUE SOLUCIONA EL ERROR:
+  output: {
+    // Si estamos en producción (Vercel), usa tu dominio real.
+    // Si estamos en local (dev), usa undefined (que por defecto es la raíz local).
+    assetPrefix: process.env.NODE_ENV === 'production' 
+      ? 'https://remotedemo.vercel.app/' 
+      : undefined,
   },
 });
